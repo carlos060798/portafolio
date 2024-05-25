fa1fa418365901aadf9fd862b501131715315f2f
carlos060798 <daniloangaritagarcia@gmail.com>
2024-05-25 10:44:28 -0500

    version final

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -2,7 +2,6 @@ export const mainMenu = [
      {link:"#", content:"Inicio", icon:"lucide:home"},
      {link:"#about", content:"Acerca de mi", icon:"octicon:person-24"},
      {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
-     {link:"#blog", content:"experiencia", icon:"mdi:blog"}
 ]
 export const menuBlog = [
      {link:"/", content:"Home", icon:"lucide:home"},
